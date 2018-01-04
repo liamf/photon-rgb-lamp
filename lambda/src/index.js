9c29be3a820c3a623851bcbe26dd7c64d52352e7
Liam Friel <liam.friel@gmail.com>
2018-01-04 00:06:49 +0000

    Adding support for the ReportState interface: Alexa app on/off and brightness status working. Colour state not for some reason.

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -1,18 +1,13 @@
 'use strict';
 
 /**
- * This is a very simple example of using the Amazon Smart Home "light" API to control the Particle "Pixel" lamp 
- * It implements power on/off, dimming, and colour control
+ * This is a very simple example of using the Amazon Smart Home "light" API to control the Particle "Pixel" lamp in the attic
  * 
  * For more information about developing smart home skills, see
  *  https://developer.amazon.com/alexa/smart-home
  *
  * For details on the smart home API, please visit
  *  https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/smart-home-skill-api-reference
- *
- * Free use for any purpose without restriction.
- *
- * (c) Liam Friel 2018
  */
 
 var httpRequest = require('request');
@@ -36,42 +31,45 @@ const LIAMS_SMART_DEVICE = [
         manufacturerName: 'Liam',
         displayCategories: ['LIGHT'],
         cookie: {
-            // This is the Particle Device ID of this lamp (you'd need to fix this to be the ID of a real lamp you can control)
-            deviceId: '3a001234550647343138xxxxx',
-            // This is the root access token for the account user
-            // For some reason the OAUTH issued tokens don't seem to work (permission denied) and the reason is not understood yet
-            rootToken: '5474c0123456789015e60e34f1a1',
+            // This is the Particle Device ID of this lamp
+            deviceId: '3a0034000647343138333038',
+            rootToken: '5474c087d755d05d7b3e90e20aa615e60e34f1a1',
         },
         capabilities: [
+            {
+                type: 'AlexaInterface',
+                interface: 'Alexa',
+                version: '3'
+            },
             {
                 type: 'AlexaInterface',
                 interface: 'Alexa.PowerController',
                 version: '3',
                 properties: {
-                    supported: [{name: 'powerState'}]
+                    supported: [{name: 'powerState'}], 
+                    proactivelyReported: false,
+                    retrievable: true
                 },
-                proactivelyReported: true,
-                retrievable: true,
             },
             {
                 type: 'AlexaInterface',
                 interface: 'Alexa.BrightnessController',
                 version: '3',
                 properties: {
-                    supported: [{name: 'brightness'}]
+                    supported: [{name: 'brightness'}],
+                    proactivelyReported: false,
+                    retrievable: true
                 },
-                proactivelyReported: true,
-                retrievable: true,
             },
             {
                 type: 'AlexaInterface',
                 interface: 'Alexa.ColorController',
                 version: '3',
                 properties: {
-                    supported: [{name: 'color'}]
+                    supported: [{name: 'color'}],
+                    proactivelyReported: false,
+                    retrievable: true
                 },
-                proactivelyReported: true,
-                retrievable: true,
             },
         ],
     },
@@ -87,6 +85,47 @@ const SV_MAX = 100
 
 function _normalizeAngle (degrees) {
 	  return (degrees % 360 + 360) % 360;
+}
+
+// Not sure where this fn came from ... 
+function rgb2hsv (r,g,b) {
+ var computedH = 0;
+ var computedS = 0;
+ var computedV = 0;
+
+ //remove spaces from input RGB values, convert to int
+ //No real sanity checking or error catching here. 
+ var r = parseInt( (''+r).replace(/\s/g,''),10 ); 
+ var g = parseInt( (''+g).replace(/\s/g,''),10 ); 
+ var b = parseInt( (''+b).replace(/\s/g,''),10 ); 
+
+// assumes our default colour ranges for the lamp
+ r=r/4095; g=g/4095; b=b/4095;
+ var minRGB = Math.min(r,Math.min(g,b));
+ var maxRGB = Math.max(r,Math.max(g,b));
+
+ // Black-gray-white
+ if (minRGB==maxRGB) {
+  computedV = minRGB;
+  return {
+      h: 0,
+      s: 0,
+      v: computedV
+    };
+ }
+
+ // Colors other than black-gray-white:
+ var d = (r==minRGB) ? g-b : ((b==minRGB) ? r-g : b-r);
+ var h = (r==minRGB) ? 3 : ((b==minRGB) ? 1 : 5);
+ computedH = 60*(h - d/(maxRGB - minRGB));
+ computedS = (maxRGB - minRGB)/maxRGB;
+ computedV = maxRGB;
+ 
+ return {
+     h: computedH,
+     s: computedS,
+     v: computedV
+     };
 }
 
 // Convert from Alexa HSB model to the Pixel RGB	
@@ -188,6 +227,7 @@ function generateControlResponse(request, payload) {
         		brightness: request.directive.payload.color.brightness, 
         	}
             break;
+            
     }
  
     var context = {
@@ -243,6 +283,74 @@ function generateControlError(name, code, description) {
     return result;
 }
 
+function generateStateResponse(request, varList) {
+    var response = {};
+    var now = new Date();
+        
+    var powerState = "ON";
+    
+    var HSV = rgb2hsv( varList.red, varList.green, varList.blue);
+    
+    if( varList.red == 0 && varList.green == 0 && varList.blue === 0 )
+    {
+        powerState = "OFF";
+    }
+    
+    response.context = {
+        properties: [
+        {
+            namespace: 'Alexa.PowerController',
+            name: 'powerState',
+            value: powerState,
+            timeOfSample: now.toISOString(),
+            uncertaintyInMilliseconds: 500 
+        },
+        {
+            namespace: 'Alexa.BrightnessController',
+            name: 'brightness',
+            value: varList.level,
+            timeOfSample: now.toISOString(),
+            uncertaintyInMilliseconds: 500             
+        },
+        {
+            namespace: 'Alexa.BrightnessController',
+            name: 'color',
+            value: {
+                hue: HSV.h.toFixed(3),
+                saturation: HSV.s.toFixed(3),
+                brightness: HSV.v.toFixed(3)
+            },
+            timeOfSample: now.toISOString(),
+            uncertaintyInMilliseconds: 500   
+        }
+        ]
+    };
+    
+    // Rest of the response is generic
+    response.event = {
+        header: {
+            messageId: generateMessageID(),
+            correlationToken: request.directive.header.correlationToken,
+            namespace: 'Alexa',
+            name: 'StateReport',
+            payloadVersion: '3'
+        },
+        endpoint: {
+            scope: {
+                type: request.directive.endpoint.scope.type,
+                token: request.directive.endpoint.scope.token
+            },    
+            endpointId: request. directive.endpoint.endpointId
+        },
+        payload: {
+        }
+    };
+    
+    log( 'DEBUG', 'StateReport is ' + JSON.stringify(response));
+    
+    return response;
+}
+
 /**
  * Mock functions to access device cloud.
  *
@@ -259,6 +367,35 @@ function getDevicesFromPartnerCloud() {
      * 
      */
     return LIAMS_SMART_DEVICE;
+}
+
+function getParticleVariable(varName, request, callbackContext) {
+    
+    // Generic parameters
+    const deviceId = request.directive.endpoint.cookie.deviceId;
+    const rootToken = request.directive.endpoint.cookie.rootToken
+    const accessToken = request.directive.endpoint.scope.token; 
+    
+    // The Particle API command data 
+    var getData = {
+        method: 'GET',
+        url: particleServer + particlePath + deviceId + "/" + varName,
+        qs: {
+        	access_token: rootToken
+        },
+    };
+
+    // log('DEBUG', 'Requesting Variable: ' + JSON.stringify(getData));
+    
+    // Request the data and callback with the result
+    httpRequest(getData, function(error, response, body) {
+        var d = JSON.parse(body) ; 
+        var result = d.result;
+        if( !error ) {
+            callbackContext.particleVars[varName] = result;
+            callbackContext.respCallback(callbackContext.particleVars)
+        } 
+    });
 }
 
 function setLightColour( commandArgs, request, context) {
@@ -291,7 +428,7 @@ function setLightColour( commandArgs, request, context) {
         },
     };
 
-    log( 'DEBUG', "command is " + JSON.stringify(postData))
+    // log( 'DEBUG', "command is " + JSON.stringify(postData))
     httpRequest.post(postData, respCallback)	
 }
 
@@ -345,7 +482,39 @@ function setBrightness(request, context) {
     setLightColour(commandArg, request, context)
 }
 
-
+function getState(request, context) {
+    
+    /**
+     * Grab the deviceId from the request.
+     */
+    const deviceId = request.directive.endpoint.cookie.deviceID;
+    const accessToken = request.directive.endpoint.scope.token;
+    
+    // Store the results here, process when we have them all
+    let particleVars = {}
+    var respCallback = function(varList) {
+        // We only execute the callback when we have all 4 variables filled in
+        if( Object.keys(varList).length == 4 )
+        {
+            // OK, we have all the variables.
+            var stateResponse = generateStateResponse(request, varList);
+            context.succeed(stateResponse);
+        }
+    };
+ 
+    // Well this looks fugly
+    var callbackContext = {
+        respCallback,
+        particleVars
+    }
+    
+    // Ask for a few variables ... and handle them in the callback
+    getParticleVariable( 'red', request, callbackContext);
+    getParticleVariable( 'green', request, callbackContext);
+    getParticleVariable( 'blue', request, callbackContext);
+    getParticleVariable( 'level', request, callbackContext);
+    
+}
 /**
  * Main logic
  */
@@ -390,6 +559,29 @@ function handleDiscovery(request, context) {
 }
 
 /**
+ * 
+ * A functiont to handle some more generic "Alexa" queries
+ * The only one we care about at the moment is "ReportState"
+ */
+ 
+function handleGenericAlexaMessage(request, context)
+{
+    log('DEBUG', 'Generic Alexa query');
+
+    switch( request.directive.header.name)
+    {
+        case 'ReportState': 
+            getState(request, context);
+            log('DEBUG', 'Reporting State');
+            break;
+            
+        default: 
+            log('ERROR', `No supported directive name: ${request.header.name}`);
+            return;        
+    }
+}
+ 
+/**
  * A function to handle control events.
  * This is called when Alexa requests an action such as turning off an appliance.
  *
@@ -398,12 +590,6 @@ function handleDiscovery(request, context) {
  */
 function handleControl(request, context) {
     log('DEBUG', `Control Request: ${JSON.stringify(request)}`);
-
-    /**
-     * Grab the applianceId from the request.
-     */
-    const deviceId = request.directive.endpoint.cookie.deviceID;
-    const accessToken = request.directive.endpoint.scope.token;
     
     // We ignore the "namespace" : the name is unique for this simple Skill
     switch (request.directive.header.name) {
@@ -420,10 +606,9 @@ function handleControl(request, context) {
             setBrightness(request, context);
             break;
 
-        case 'SetColor': {
+        case 'SetColor': 
             setColor(request, context);
             break;
-        }
 
         default: {
             log('ERROR', `No supported directive name: ${request.header.name}`);
@@ -444,13 +629,13 @@ function handleControl(request, context) {
 exports.handler = (request, context) => {
     
     // Dump out the messages received
-    log('DEBUG',JSON.stringify(request));
+    log('DEBUG', 'REQUEST was: ' + JSON.stringify(request));
     log('DEBUG', JSON.stringify(context));
     
     
     switch (request.directive.header.namespace) {
         /**
-         * The namespace of 'Alexa.Discovery' indicates a request is being made to the Lambda for
+         * The namespace of 'Alexa.ConnectedHome.Discovery' indicates a request is being made to the Lambda for
          * discovering all appliances associated with the customer's appliance cloud account.
          *
          * For more information on device discovery, please see
@@ -470,16 +655,11 @@ exports.handler = (request, context) => {
             handleControl(request, context);
             break;
 
-        /**
-         * The namespace of "Alexa.Query" indicates a request is being made to query devices about
-         * information like temperature or lock state. The full list of Query events sent to your lambda are described below.
-         *  https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/smart-home-skill-api-reference#payload
-         *
-         * TODO: In this sample, query handling is not implemented. Implement it to retrieve temperature or lock state.
-         */
-        // case 'Alexa.Query':
-        //     handleQuery(request, context);
-        //     break;
+        // Handler for the ReportState interface
+        case 'Alexa':
+            handleGenericAlexaMessage(request, context);
+            break;
+            
 
         /**
          * Received an unexpected message
